7abc0961306ef9823d9f694e091bad6236d0dfa1
thiagomorgado <thiagomorgado0@icloud.com>
2021-04-21 23:00:16 -0300

    Inicial Project

--- api/src/server.ts
+++ api/src/server.ts
@@ -2,6 +2,18 @@
 // Instalando tabem as tipagem do da lib do express 
 import express from 'express';
 
+// criando variavel server e atribuindo as funcionalidade do express
 const server = express();
+
+
+// aqui esta criando rota ger e retornado um json
+server.get('/user', (request, response) => {
+  return(
+    response.json({
+      messager: 'HOla thiago'
+    })
+  )
+})
+
 
 server.listen(3333)
