d7beb40ba748be8736eca8b5a8d4b1d1339b632b
thiagomorgado <thiagomorgado0@icloud.com>
2021-04-21 22:49:10 -0300

    Inicial Project

---
+++ api/src/server.ts
@@ -0,0 +1,7 @@
+// Aqui esta importando o express no typescript 
+// Instalando tabem as tipagem do da lib do express 
+import express from 'express';
+
+const server = express();
+
+server.listen(3333)
