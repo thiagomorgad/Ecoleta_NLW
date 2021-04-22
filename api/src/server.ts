2b26031698102f8522e58d0671f07e2d1ec80513
thiagomorgado <thiagomorgado0@icloud.com>
2021-04-21 23:09:42 -0300

    inicial project

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,19 +1,14 @@
 // Aqui esta importando o express no typescript 
 // Instalando tabem as tipagem do da lib do express 
 import express from 'express';
-
 // criando variavel server e atribuindo as funcionalidade do express
 const server = express();
-
+  const listagemName  = ['thiago', "aline", 'Fernando', 'alanna']
 
 // aqui esta criando rota ger e retornado um json
 server.get('/user', (request, response) => {
-  return(
-    response.json({
-      messager: 'HOla thiago'
-    })
-  )
+  response.json(listagemName)
 })
 
 
-server.listen(3333)
+server.listen(3333);
